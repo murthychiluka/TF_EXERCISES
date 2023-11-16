ed10457e4c3c0d63bd5bc77445307c7d2500f45c
murthychiluka <murthychiluka@gmail.com>
2023-11-16 21:54:26 +0530

    c

--- exercise1/first_instance.tf
+++ exercise1/first_instance.tf
@@ -1,18 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-  #   access_key = ""
-  #   secret_key = ""	
-}
-
 resource "aws_instance" "ec2" {
   ami                    = "ami-03265a0778a880afb"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
-  vpc_security_group_ids =  ["sg-0e05adc3a9fdbddf0"]
+  vpc_security_group_ids = ["sg-0e05adc3a9fdbddf0"]
   tags = {
     name = "test"
   }
-  
-  
-  }
+}
 
