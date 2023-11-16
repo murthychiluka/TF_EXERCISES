774c1a296790030f579ca00a697343880288b3d9
murthychiluka <murthychiluka@gmail.com>
2023-11-16 21:25:04 +0530

    d

--- exercise1/first_instance.tf
+++ exercise1/first_instance.tf
@@ -4,10 +4,14 @@ provider "aws" {
   #   secret_key = ""	
 }
 
-resource "aws_instance" "intro" {
+resource "aws_instance" "ec2" {
   ami                    = "ami-03265a0778a880afb"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
+  vpc_security_group_ids =  ["sg-0e05adc3a9fdbddf0"]
+  tags = {
+    name = "test"
+  }
   
   
   }
