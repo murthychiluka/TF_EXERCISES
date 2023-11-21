9999f507d499bc8c8bbff19f3b1c30b440946201
murthychiluka <murthychiluka@gmail.com>
2023-11-21 13:25:36 +0530

    backend

--- exercise5/backend.tf
+++ exercise5/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terra-state-dove"
+    bucket = "tera-state-murthy"
     key    = "terraform/backend"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
